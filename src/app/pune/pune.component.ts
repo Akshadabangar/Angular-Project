df0f2acb0ca04e43cfee05752b010a39d72ed0d1
Akshadabangar <akshadabangar21052000@gmail.com>
2024-06-16 12:33:10 +0530

    adding image pune

--- src/app/pune/pune.component.ts
+++ src/app/pune/pune.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class PuneComponent {
 
   
-
-
+akshadaurl="https://tse1.mm.bing.net/th?id=OIP.cov40pcUyl5AAs0k8MRVawHaLJ&pid=Api&P=0&h=180";
+sonaliurl="https://tse1.mm.bing.net/th?id=OIP.q6hS-0RWK8iZcQsx1uYAmgHaLD&pid=Api&P=0&h=180";
+sayaliurl="https://i.pinimg.com/originals/51/fe/18/51fe180cc453fccbf05652ad051b4803.jpg";
 }
